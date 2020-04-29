84e1bc2cece61b771326784cb8c051462159ccbd
Alex G <4lex.tristan@gmail.com>
2020-04-29 22:10:50 +0200

    update some images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,23 +41,27 @@
       <v-container fluid primary lighten-1>
         <h1 class="text-center white--text">Pourquoi utiliser Kewz?</h1>
         <v-row>
-          <v-col cols="12" sm="6" md="4" align="center">
+          <v-col cols="12" sm="6" md="4" lg="2" align="center">
             <v-img alt="gestion de l'attente logo" contain src="../assets/distanciation-logo.svg" width="100"></v-img>
-            <h3 class="text-center white--text">Distanciation sociale</h3>
+            <h3 class="text-center white--text">Respecter la distanciation physique</h3>
           </v-col>
-          <v-col cols="12" sm="6" md="4" align="center">
+          <v-col cols="12" sm="6" md="4" lg="2" align="center">
             <v-img alt="gestion de l'attente logo" contain src="../assets/attente-logo.svg" width="100"></v-img>
-            <h3 class="text-center white--text">Gestion de l'attente</h3>
+            <h3 class="text-center white--text">Faciliter la gestion de l'attente</h3>
           </v-col>
-          <v-col cols="12" sm="6" md="4" align="center">
+          <v-col cols="12" sm="6" md="4" lg="2" align="center">
             <v-img alt="simple logo" contain src="../assets/simple-logo.svg" width="100"></v-img>
-            <h3 class="text-center white--text">Simple</h3>
+            <h3 class="text-center white--text">Simplifier l'utilisation</h3>
           </v-col>
-          <v-col cols="12" sm="6" md="6" align="center">
+          <v-col cols="12" md="4" lg="2" align="center">
+            <v-img alt="Open source logo" contain src="../assets/multi-logo.svg" width="100"></v-img>
+            <h3 class="text-center white--text">Multisupport</h3>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" lg="2" align="center">
             <v-img alt="free logo" contain src="../assets/free-logo.svg" width="100"></v-img>
             <h3 class="text-center white--text">Gratuit</h3>
           </v-col>
-          <v-col cols="12" md="6" align="center">
+          <v-col cols="12" md="4" lg="2" align="center">
             <v-img alt="Open source logo" contain src="../assets/opensource-logo.svg" width="100"></v-img>
             <h3 class="text-center white--text">Open source</h3>
           </v-col>
